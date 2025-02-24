ed1a9a3353f4328f8ea497ca1edd21b5ed1706a3
arshtyi <640006128@qq.com>
2025-02-24 17:59:49 +0800

    update

--- src/home/1163/main.cpp
+++ src/home/1163/main.cpp
@@ -1,40 +1,30 @@
-#define LOCAL
-#include <iostream>
-#include <vector>
-#include <algorithm>
+// 不开long long一场空--2025/2/15
+#include <bits/stdc++.h>
 using namespace std;
-
-struct vec
+struct Node
 {
-    int x,y;
-    vec (int x = 0,int y = 0):x(x),y(y) {}
+    long long x,y;
+    Node (long long x = 0,long long y = 0):x(x),y(y) {}
 };
-
-bool less_cmp(const vec &a,const vec &b){
-    return a.x + a.y < b.x + b.y;
+void solve(){
+    long long n;cin >> n;
+    vector<Node> v;
+    for(long long i = 1;i<=n;i++){
+        long long x,y;cin >> x>> y;
+        v.push_back(Node(x,y));
+    }
+    stable_sort(v.begin(), v.end(), [](const Node &a, const Node &b) {
+        return (a.x + a.y) < (b.x + b.y);
+    });
+    for(auto h : v){
+        cout << h.x << " " << h.y << endl;
+    }
 }
-
- main(){
-    #ifdef LOCAL
-        freopen("input.txt","r",stdin);
-        freopen("output.txt","w",stdout);
-    #endif
-    int T,n;cin >> T;
-    vector<vec> VEC[T];
-    for (int i = 0;i < T;i ++)
-    {   
-        cin >> n;int a,b;
-        for(int j = 0;j < n;j ++){
-            cin >> a >> b;
-            VEC[i].push_back(vec(a,b));
-        }
-        stable_sort(VEC[i].begin(),VEC[i].end(),less_cmp);
+signed main(){
+    long long T; cin >> T;
+    while (T--)
+    {
+        solve();
     }
-    for(const auto &SO:VEC){
-        for(const auto &so : SO){
-            cout << so.x << " " << so.y << endl;
-        }
-    }
-    
     return 0;
 }
