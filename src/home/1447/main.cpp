b5b794739544666750058b7d00edc4720de61ef0
arshtyi <640006128@qq.com>
2025-03-01 17:19:52 +0800

    update

--- src/home/1447/main.cpp
+++ src/home/1447/main.cpp
@@ -1,61 +1,60 @@
-#define LOCAL
 #include <bits/stdc++.h>
 using namespace std;
-pair<vector<int>, vector<int>> slidingWindowMaxMin(const vector<int>& nums, int K) {
-    int n = nums.size();
-    if (n == 0 || K == 0) {
-        return {{}, {}}; 
-    }
-    
-    deque<int> maxDeque, minDeque; // 双端队列用于维护最大值和最小值
-    vector<int> maxResult, minResult; // 存储每次滑动窗口的最大值和最小值
-    
-    for (int i = 0; i < n; ++i) {
-        // 移除不在当前窗口的元素
-        if (!maxDeque.empty() && maxDeque.front() < i - K + 1) {
-            maxDeque.pop_front();
-        }
-        if (!minDeque.empty() && minDeque.front() < i - K + 1) {
-            minDeque.pop_front();
-        }
+using ll = long long;
+const int N = 1e6 + 100;
+struct Node
+{
+    ll num, val;
+    Node(ll num, ll val) : num(num), val(val) {}
+};
 
-        // 维护最大值队列，确保队列中的元素从大到小
-        while (!maxDeque.empty() && nums[maxDeque.back()] <= nums[i]) {
-            maxDeque.pop_back();
+deque<Node> minq, maxq;
+vector<ll> minv, maxv;
+signed main()
+{
+    cin.tie(0);
+    cout.tie(0);
+    ios::sync_with_stdio(0);
+    int n, k;
+    cin >> n >> k;
+    for (int i = 1; i <= n; i++)
+    {
+        int x;
+        cin >> x;
+        Node t(i, x);
+        while (!minq.empty() && minq.back().val >= t.val)
+        {
+            minq.pop_back();
         }
-        maxDeque.push_back(i);
-
-        // 维护最小值队列，确保队列中的元素从小到大
-        while (!minDeque.empty() && nums[minDeque.back()] >= nums[i]) {
-            minDeque.pop_back();
+        minq.push_back(t);
+        while (!maxq.empty() && maxq.back().val <= t.val)
+        {
+            maxq.pop_back();
         }
-        minDeque.push_back(i);
-
-        // 当窗口大小达到 K 时，记录当前窗口的最大值和最小值
-        if (i >= K - 1) {
-            maxResult.push_back(nums[maxDeque.front()]);
-            minResult.push_back(nums[minDeque.front()]);
+        maxq.push_back(t);
+        while (minq.front().num + k <= t.num)
+        {
+            minq.pop_front();
+        }
+        while (maxq.front().num + k <= t.num)
+        {
+            maxq.pop_front();
+        }
+        if (i >= k)
+        {
+            minv.push_back(minq.front().val);
+            maxv.push_back(maxq.front().val);
         }
     }
-    
-    return {maxResult, minResult};
-}
-signed main(){
-    #ifdef LOCAL
-        freopen("data.in","r",stdin);
-        freopen("data.out","w",stdout);
-    #endif
-    int n,k;cin >> n >> k;
-    vector<int> window(n);
-    for(int i = 0;i < n;i ++){
-        cin >> window[i];
+    for (auto v : minv)
+    {
+        cout << v << " ";
     }
-    auto [maxResult,minResult] = slidingWindowMaxMin(window,k);
-    for(auto minVal : minResult){
-        cout << minVal << " ";
-    }cout << endl;
-    for(auto maxVal : maxResult){
-        cout << maxVal << " ";
-    }cout << endl;
+    cout << "\n";
+    for (auto v : maxv)
+    {
+        cout << v << " ";
+    }
+    cout << "\n";
     return 0;
 }
