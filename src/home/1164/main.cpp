afe94399509ba3f7be39cdf4bc4d7c4c18085e85
arshtyi <640006128@qq.com>
2025-03-04 21:01:17 +0800

    update

---
+++ src/home/1164/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+const int N = 1e6;
+void solve()
+{
+}
+signed main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        solve();
+    }
+
+    return 0;
+}
