a1b751cbab8287507aae81015be1c4ee0cbc3d36
Arshtyi <arshtyi_trantor@outlook.com>
2025-04-12 11:42:17 +0800

    update newline

--- src/home/1005/main.cpp
+++ src/home/1005/main.cpp
@@ -1,15 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-ll n,x,s;
-int main(){
+ll n, x, s;
+int main()
+{
     cin >> n;
     while (cin >> n)
     {
-        s=0;
+        s = 0;
         while (n--)
         {
-            cin >> x;s+=x;
+            cin >> x;
+            s += x;
         }
         cout << s << endl;
     }
